f72037fb5f98da34c3c5533abdc01c860da5a789
zhangyang <zhangyang@zhiyuanhui.com>
2019-09-03 16:41:17 +0800

    them

--- src/element-ui-theme/index.js
+++ src/element-ui-theme/index.js
@@ -12,7 +12,7 @@
  *  1. 此处只包含element-ui组件主题
  *  2. 要达到整站主题修改效果, 请确保[import './element-[#17B3A3]/index.css']当前主题色与[/src/assets/scss/_variables.scss]文件中[$--color-primary]属性值一致
  */
-import './element-#3E8EF7/index.css' // 当前主题色
+import './element-#00A6FF/index.css' // 当前主题色
 export default {
   list: [
     '#0BB2D4', // 青色
@@ -26,6 +26,7 @@ export default {
     '#EB6709', // 橙色
     '#F74584', // 粉红色
     '#FCB900', // 黄色
-    '#FF4C52'  // 红色
+    '#FF4C52',  // 红色
+    '#00A6FF'
   ]
 }
