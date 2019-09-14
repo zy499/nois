9ac45a34d2c9ea5c962f3d0ee9437890e5ee961e
zy499 <hqh499@gmail.com>
2019-09-14 18:03:05 +0800

    预计今日

--- src/components/passengerFlow/ChartsXWCX.vue
+++ src/components/passengerFlow/ChartsXWCX.vue
@@ -1,174 +1,413 @@
 <!--
  * @Description: file content
  * @Author: zy
- * @Date: 2019-09-11 14:48:56
+ * @Date: 2019-09-09 22:32:42
  * @LastEditors: zy
- * @LastEditTime: 2019-09-11 15:05:05
+ * @LastEditTime: 2019-09-14 17:23:24
  -->
 <template>
-  <ve-line :data="chartData" :extend="chartsextend"></ve-line>
+  <div>
+    <div :id="id" :class="className" :style="{height:height,width:width}" />
+  </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+import resize from '../resize'
+var rows = [
+  {
+    时间: '05:00',
+    进站量: 3489,
+    出站量: 1489,
+    换乘量: 2489,
+    客运量: 12541
+  },
+  {
+    时间: '06:00',
+    进站量: 6489,
+    出站量: 7489,
+    换乘量: 9489,
+    客运量: 22541
+  },
+  {
+    时间: '07:00',
+    进站量: 9489,
+    出站量: 8489,
+    换乘量: 8489,
+    客运量: 32541
+  },
+  {
+    时间: '08:00',
+    进站量: 41489,
+    出站量: 36589,
+    换乘量: 10489,
+    客运量: 45632
+  },
+  {
+    时间: '09:00',
+    进站量: 22978,
+    出站量: 20989,
+    换乘量: 9489,
+    客运量: 44444
+  },
+  {
+    时间: '10:00',
+    进站量: 13567,
+    出站量: 9489,
+    换乘量: 7000,
+    客运量: 20354
+  },
+  {
+    时间: '11:00',
+    进站量: 12456,
+    出站量: 8489,
+    换乘量: 10489,
+    客运量: 19563
+  },
+  {
+    时间: '12:00',
+    进站量: 35632,
+    出站量: 20489,
+    换乘量: 11189,
+    客运量: 40639
+  },
+  {
+    时间: '13:00',
+    进站量: 28945,
+    出站量: 19889,
+    换乘量: 10489,
+    客运量: 33333
+  },
+  {
+    时间: '14:00',
+    进站量: 14890,
+    出站量: 13689,
+    换乘量: 12189,
+    客运量: 19820
+  },
+  {
+    时间: '15:00',
+    进站量: 19870,
+    出站量: 10489,
+    换乘量: 16987,
+    客运量: 22222
+  },
+  {
+    时间: '16:00',
+    进站量: 16541,
+    出站量: 11089,
+    换乘量: 9999,
+    客运量: 19802
+  },
+  {
+    时间: '17:00',
+    进站量: 36541,
+    出站量: 26089,
+    换乘量: 18997,
+    客运量: 33365
+  },
+  {
+    时间: '18:00',
+    进站量: 65423,
+    出站量: 36589,
+    换乘量: 32511,
+    客运量: 73250
+  },
+  {
+    时间: '19:00',
+    进站量: 41230,
+    出站量: 40039,
+    换乘量: 12365,
+    客运量: 65201
+  },
+  {
+    时间: '20:00',
+    进站量: 33333,
+    出站量: 31489,
+    换乘量: 19365,
+    客运量: 32415
+  },
+  {
+    时间: '21:00',
+    进站量: 31258,
+    出站量: 29489,
+    换乘量: 11111,
+    客运量: 40304
+  },
+  {
+    时间: '22:00',
+    进站量: 12365,
+    出站量: 9489,
+    换乘量: 8603,
+    客运量: 22310
+  },
+  {
+    时间: '23:00',
+    进站量: 9365,
+    出站量: 7489,
+    换乘量: 4603,
+    客运量: 12310
+  },
+  {
+    时间: '24:00',
+    进站量: 2365,
+    出站量: 6489,
+    换乘量: 2603,
+    客运量: 6310
+  }
+]
+var dataIPSxAxis = []
+var dataJz = []
+var dataCz = []
+var dataHc = []
+var dataKy = []
+rows.forEach(item => {
+  dataIPSxAxis.push(item['时间'])
+  dataJz.push(item['进站量'])
+  dataCz.push(item['出站量'])
+  dataHc.push(item['换乘量'])
+  dataKy.push(item['客运量'])
+})
 export default {
-  data: function () {
-    // const self = this
+  mixins: [resize],
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    id: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '400px'
+    }
+  },
+  data () {
     return {
+      chart: null,
+      showDate: ''
+    }
+  },
+  mounted () {
+    this.initChart()
+  },
+  beforeDestroy () {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    initChart () {
+      this.chart = echarts.init(document.getElementById(this.id))
+      this.chart.setOption({
+        backgroundColor: '#fff',
+        tooltip: {
+          trigger: 'axis',
+          backgroundColor: '#fff',
+          borderWidth: 1,
+          borderColor: '#E8ECEF',
+          axisPointer: {
+            lineStyle: {
+              color: '#57617B'
+            }
+          },
+          textStyle: {
+            color: '#556270',
+            fontSize: '12px'
+          },
+          formatter (param) {
+            return `<div style="width:160px;">
+                      <p style="border-bottom:1px solid rgba(232,236,239,1); padding-left:10px;">${param[0].axisValue}</p>
+                      <p style="border-bottom:1px solid rgba(232,236,239,1);padding:10px 0">
+                        <span style="display: inline-block;width:10px;height:10px;background-color:#3BD19F;border-radius:5px;margin:0 6px"></span>
+                        <span>${param[0].seriesName}:&nbsp;&nbsp;${param[0].value}</span>
+                        <span></span>
+                      </p>
+                       <p style="border-bottom:1px solid rgba(232,236,239,1);padding:10px 0">
+                        <span style="display: inline-block;width:10px;height:10px;background-color:#FF2F78;border-radius:5px;margin:0 6px"></span>
+                        <span>${param[1].seriesName}:&nbsp;&nbsp;${param[1].value}</span>
+                        <span></span>
+                      </p>
+                       <p style="border-bottom:1px solid rgba(232,236,239,1);padding:10px 0">
+                        <span style="display: inline-block;width:10px;height:10px;background-color:#3553FF;border-radius:5px;margin:0 6px"></span>
+                        <span>${param[2].seriesName}:&nbsp;&nbsp;${param[2].value}</span>
+                        <span></span>
+                      </p>
+                       <p style="padding:10px 0">
+                        <span style="display: inline-block;width:10px;height:10px;background-color:#FA852E;border-radius:5px;margin:0 6px"></span>
+                        <span>${param[3].seriesName}:&nbsp;&nbsp;${param[3].value}</span>
+                        <span></span>
+                      </p>
+                    </div>`
+          },
+          rich: {
+            a: {
 
-      chartsextend: {
+            }
+          }
+        },
         legend: {
-          icon: 'circle', // circle，rect ，roundRect，triangle，diamond，pin，arrow，none
-          itemWidth: 8, // 设置宽度
-          itemHeight: 8, // 设置高度
-          //   itemGap: 40 // 设置间距
-          right: 20
+          icon: 'circle',
+          itemWidth: 10,
+          itemHeight: 10,
+          data: ['进站量', '出站量', '换乘量', '客运量'],
+          right: '2%',
+          textStyle: {
+            fontSize: 14,
+            color: '#354052',
+            padding: [0, 20, 0, 0]
+          }
         },
         grid: {
-          left: 20,
-          top: 30,
-          bottom: '1%',
-          y2: 10
-          // right:'10%'
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
         },
-        series: {
-          Symbol: 'none',
-          smoth: false,
-          showSymbol: false
-        }
-        // tooltip (v) {
-        //   // axisPointer为直线 可设置为shadow
-        //   // v.axisPointer ={
-        //   //     type:'line'
-        //   // }
-        //   v.formatter = function ([params]) {
-        //     const str = `
-        //             <div style="background:#fff;border:1px solid #E8ECEF">
-        //               <p style="margin:0;padding:0;color:#556270;border-bottom:1px solid #F0F4F7">${self.chartData.rows[params.dataIndex]['时间']}</p>
-        //               <span style="display:inline-block">${self.chartData.columns[1]}:${self.chartData.rows[params.dataIndex]['进站量']}</span>
-        //             </div>
-        //             `
-        //     return str
-        //   }
-        //   return v
-        // }
-      },
-      chartData: {
-        columns: ['时间', '进站量', '出站量', '换乘量', '客运量'],
-        rows: [
+        xAxis: [
           {
-            时间: '07:00',
-            进站量: 9489,
-            出站量: 8489,
-            换乘量: 8489,
-            客运量: 32541
+            type: 'category',
+            boundaryGap: false,
+            axisLine: {
+              lineStyle: {
+                color: 'rgba(148, 158, 168, 1)'
+              }
+            },
+            axisTick: {
+              show: false
+            },
+            axisLabel: {
+              color: '#000'
+            },
+            data: dataIPSxAxis
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            axisTick: {
+              show: false
+            },
+            axisLine: {
+              show: false
+            },
+            axisLabel: {
+              margin: 20,
+              textStyle: {
+                fontSize: 12,
+                color: 'rgba(148, 158, 168, 1)'
+              }
+            },
+            splitLine: {
+              lineStyle: {
+                type: 'dashed'
+              }
+            }
+          }
+        ],
+        series: [
+          {
+            name: '进站量',
+            type: 'line',
+            smooth: true,
+            symbol: 'circle',
+            symbolSize: 5,
+            showSymbol: false,
+            lineStyle: {
+              normal: {
+                width: 1
+              }
+            },
+            itemStyle: {
+              normal: {
+                color: 'rgba(59, 209, 159, 1)',
+                borderWidth: 16
+              }
+            },
+            data: dataJz
           },
           {
-            时间: '08:00',
-            进站量: 41489,
-            出站量: 36589,
-            换乘量: 10489,
-            客运量: 45632
+            name: '出站量',
+            type: 'line',
+            smooth: true,
+            symbol: 'circle',
+            symbolSize: 5,
+            showSymbol: false,
+            lineStyle: {
+              normal: {
+                width: 1
+              }
+            },
+            itemStyle: {
+              normal: {
+                color: 'rgba(255, 47, 120, 1)',
+                borderWidth: 16
+              }
+            },
+            data: dataCz
           },
           {
-            时间: '09:00',
-            进站量: 22978,
-            出站量: 20989,
-            换乘量: 9489,
-            客运量: 44444
+            name: '换乘量',
+            type: 'line',
+            smooth: true,
+            symbol: 'circle',
+            symbolSize: 5,
+            showSymbol: false,
+            lineStyle: {
+              normal: {
+                width: 1
+              }
+            },
+            areaStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                  offset: 0,
+                  color: 'rgba(53, 83, 255, .6)'
+                }, {
+                  offset: 1,
+                  color: 'rgba(255,255,255,0.6)'
+                }], false),
+                opacity: 0.3
+              }
+            },
+            itemStyle: {
+              normal: {
+                color: 'rgba(53, 83, 255, 1)',
+                borderWidth: 16
+              }
+            },
+            data: dataHc
           },
           {
-            时间: '10:00',
-            进站量: 13567,
-            出站量: 9489,
-            换乘量: 7000,
-            客运量: 20354
-          },
-          {
-            时间: '11:00',
-            进站量: 12456,
-            出站量: 8489,
-            换乘量: 10489,
-            客运量: 19563
-          },
-          {
-            时间: '12:00',
-            进站量: 35632,
-            出站量: 20489,
-            换乘量: 11189,
-            客运量: 40639
-          },
-          {
-            时间: '13:00',
-            进站量: 28945,
-            出站量: 19889,
-            换乘量: 10489,
-            客运量: 33333
-          },
-          {
-            时间: '14:00',
-            进站量: 14890,
-            出站量: 13689,
-            换乘量: 12189,
-            客运量: 19820
-          },
-          {
-            时间: '15:00',
-            进站量: 19870,
-            出站量: 10489,
-            换乘量: 16987,
-            客运量: 22222
-          },
-          {
-            时间: '16:00',
-            进站量: 16541,
-            出站量: 11089,
-            换乘量: 9999,
-            客运量: 19802
-          },
-          {
-            时间: '17:00',
-            进站量: 36541,
-            出站量: 26089,
-            换乘量: 18997,
-            客运量: 33365
-          },
-          {
-            时间: '18:00',
-            进站量: 65423,
-            出站量: 36589,
-            换乘量: 32511,
-            客运量: 73250
-          },
-          {
-            时间: '19:00',
-            进站量: 41230,
-            出站量: 40039,
-            换乘量: 12365,
-            客运量: 65201
-          },
-          {
-            时间: '20:00',
-            进站量: 33333,
-            出站量: 31489,
-            换乘量: 19365,
-            客运量: 32415
-          },
-          {
-            时间: '21:00',
-            进站量: 31258,
-            出站量: 29489,
-            换乘量: 11111,
-            客运量: 40304
-          },
-          {
-            时间: '22:00',
-            进站量: 12365,
-            出站量: 9489,
-            换乘量: 8603,
-            客运量: 22310
+            name: '客运量',
+            type: 'line',
+            smooth: true,
+            symbol: 'circle',
+            symbolSize: 5,
+            showSymbol: false,
+            lineStyle: {
+              normal: {
+                width: 1
+              }
+            },
+            itemStyle: {
+              normal: {
+                color: 'rgba(250, 133, 46, 1)',
+                borderWidth: 16
+              }
+            },
+            data: dataKy
           }
         ]
-      }
+      })
     }
   }
 }
--- src/components/passengerFlow/passengerWarning/TodayEstimate.vue
+++ src/components/passengerFlow/passengerWarning/TodayEstimate.vue
@@ -3,50 +3,42 @@
  * @Author: zy
  * @Date: 2019-08-31 01:42:29
  * @LastEditors: zy
- * @LastEditTime: 2019-09-11 14:52:42
+ * @LastEditTime: 2019-09-14 15:47:56
  -->
 <template>
   <div class="dialogBox">
-    <el-dialog title="预计今日" :visible.sync="dialogVisible" @close="closeDialog" width="60%">
-      <div class="dialog-top">
-        <el-row :gutter="20">
-          <el-col :span="4">
-            <p class="goOn margin-none">线网出行量</p>
-            <h4 class="num margin-none">3,024,162</h4>
+    <el-dialog title="预计今日" :visible.sync="dialogVisible" @close="closeDialog" width="80%">
+      <div class="dialog-main">
+        <el-row :gutter="20" class="margin-bottom40">
+          <el-col :span="6">
+            <div>
+              <span>线网出行量</span>
+              <span>3,024,162</span>
+            </div>
           </el-col>
           <el-col :span="6">
-            <p class="goOn margin-none">线网出站量</p>
-            <h4 class="num margin-none">3,024,162</h4>
+            <div>
+              <span>线网出行量</span>
+              <span>3,024,162</span>
+            </div>
           </el-col>
           <el-col :span="6">
-            <p class="goOn margin-none">线网换乘量</p>
-            <h4 class="num margin-none">3,024,162</h4>
+            <div>
+              <span>线网出行量</span>
+              <span>3,024,162</span>
+            </div>
           </el-col>
           <el-col :span="6">
-            <p class="goOn margin-none">线网客运量</p>
-            <h4 class="num margin-none">3,024,162</h4>
+            <div>
+              <span>线网出行量</span>
+              <span>3,024,162</span>
+            </div>
           </el-col>
         </el-row>
-      </div>
-      <div style="border: 0.5px solid gray;marginTop:30px"></div>
-      <div class="dialog-main">
-        <div>
-          <span class="margin_right_20">维度</span>
-          <el-select v-model="xianwang_val" placeholder="请选择">
-            <el-option
-              v-for="item in xianwang_option"
-              :label="item.label"
-              :value="item.value"
-              :key="item.value"
-            ></el-option>
-          </el-select>
-        </div>
         <div class="charts">
-          <p>09.24之后为测试数据</p>
           <div>
             <ChartsXWCX id="testBar" height="400px" width="100%" />
           </div>
-          
         </div>
       </div>
     </el-dialog>
@@ -116,28 +108,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.margin-none {
-  margin: 0;
-}
-.dialog-top {
-  padding: 0 30px;
-  .goOn {
-    text-align: right;
-    font-size: 12px;
-  }
-  .num {
-    text-align: right;
-    font-size: 16px;
-  }
-}
 .dialogBox {
   /deep/ .el-dialog__header {
-    text-align: center;
+    padding: 40px 20px 20px 20px;
+    border-bottom: 2px solid #e8ecef;
+    .el-dialog__headerbtn {
+      top: 38px;
+      font-size: 24px;
+    }
+  }
+  /deep/ .el-dialog__title {
+    font-size: 20px;
+    font-weight: bold;
   }
   .dialog-main {
-    margin: 20px 0px;
-    span {
-      font-size: 12px;
+    /deep/ .el-col {
+      border-right: 1px solid #e8ecef;
+      display: flex;
+      justify-content: center;
+      div {
+        display: flex;
+        flex-direction: column;
+        text-align: right;
+        span:first-child {
+          font-size: 12px;
+          color: #556270;
+          padding-bottom: 10px;
+        }
+        span:last-child {
+          font-size: 28px;
+          font-weight: bold;
+        }
+      }
+    }
+    /deep/ .el-col:last-child {
+      border-right: none;
     }
     .charts {
       .chart-box {
